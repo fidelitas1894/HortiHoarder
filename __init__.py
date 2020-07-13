3125a5d2fdb49d787e5db949e1d1a4ed580c32fc
Christian Jünger <chnjuenger@gmail.com>
2020-07-13 04:16:25 +0200

    inconsistend wording from ggg fixed

--- __init__.py
+++ __init__.py
@@ -49,8 +49,15 @@ with requests.Session() as s:
                         elif re.search("Augment an item with a new .* with Lucky values \(\d\d\)",clean_string):
                             string = clean_string.replace("Augment an item with a new ","").replace("with Lucky values ","")
                             luckyaug.append(string)
-                        elif re.search("Augment an item with a new .*",clean_string):
+                        elif re.search("Augment a Magic or Rare item with a new .* with Lucky values \(\d\d\)", clean_string):
+                            string = clean_string.replace("Augment a Magic or Rare item with a new ", "").replace(
+                                "with Lucky values ", "").replace("modifier","")
+                            luckyaug.append(string)
+                        elif re.search("Augment an item with .*",clean_string):
                             string = clean_string.replace("Augment an item with a new ","").replace("modifier","")
+                            aug.append(string)
+                        elif re.search("Augment a Magic or Rare with a new .*", clean_string):
+                            string = clean_string.replace("Augment a Magic or Rare with a new ", "").replace("modifier", "")
                             aug.append(string)
                         elif re.search("Augment a Rare item with a new modifier, with Lucky modifier values \(\d\d\)",clean_string):
                             string = clean_string.replace("Augment a Rare item with a new modifier, with","").replace("modifier values","")
